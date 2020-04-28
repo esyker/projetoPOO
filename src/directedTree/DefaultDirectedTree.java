22f521a8c0e9cf87a2806c700adb5679689a6b25
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-04-28 02:19:57 +0100

    Trees brutas

--- src/directedTree/DefaultDirectedTree.java
+++ src/directedTree/DefaultDirectedTree.java
@@ -1,48 +1,86 @@
 package directedTree;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import graph.*;
 
 public class DefaultDirectedTree<V> implements DirectedTree<V> {
 
 	TreeNode<V> root;
 
-	public DefaultDirectedTree() {
-		// TODO - implement DefaultDirectedTree.DefaultDirectedTree
-		throw new UnsupportedOperationException();
-	}
-
-	@Override
-	public void addRoot(V vertex) {
-		// TODO Auto-generated method stub
-		
+	public DefaultDirectedTree(V vertex) {
+		root = new TreeNode<V>(vertex);
 	}
 
 	@Override
 	public void addChild(V parent, V child) {
-		// TODO Auto-generated method stub
+		
+		TreeNode<V> parentNode=findTreeNode(root, parent);
+		parentNode.addChild(child);
 		
 	}
-
+	
+	TreeNode<V> findTreeNode(TreeNode<V> iter, V find) {
+		TreeNode<V> aux=iter;
+		TreeNode<V> res=null;
+		List<V> vertix = new ArrayList<V>();
+		int i = aux.children.size();
+		
+		if (find.equals(aux.vertex))
+		{
+			return aux;
+		}
+		//else
+		for (int j=0; j < i; j++)
+		{
+			vertix.add(aux.children.get(j).vertex);
+			if (find.equals(vertix.get(j)))
+			{
+				res=aux.children.get(j);
+				return res;
+			}
+			else
+			{
+				res=findTreeNode(aux, find);
+			}
+		}
+		
+		return res;
+	}
+	
+	
 	@Override
-	public V[] getChildren(V parent) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<V> getChildren(V parent) {
+		TreeNode<V> parentNode=null;
+		List<V> vertix = new ArrayList<V>();
+		
+		parentNode=findTreeNode(root, parent);
+		if (parentNode ==null) return null;
+		int i = parentNode.children.size();
+		
+		for (int j=0; j < i; j++)
+		{
+			vertix.add(parentNode.children.get(j).vertex);
+		}
+		
+		return vertix;
 	}
 
 	@Override
 	public V getRoot() {
 		// TODO Auto-generated method stub
-		return null;
+		return root.vertex;
 	}
 
 	@Override
-	public void getParent(Object child) {
+	public void getParent(V child) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void removeNode(Object vertex) {
+	public void removeNode(V vertex) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -52,6 +90,22 @@ public class DefaultDirectedTree<V> implements DirectedTree<V> {
 		// TODO Auto-generated method stub
 		
 	}
+		
+/*
+public static void main (String [] args) {
+		
+		DefaultDirectedTree<String> tree0 =  new DefaultDirectedTree<String>("root");
+	    
+		tree0.addChild("root","child1");
+		tree0.addChild("root","child2");
+		tree0.addChild("child1","child3");
 
-
+		//TreeNode<String> child1 = tree0.findTreeNode(tree0.root, "child1");
+		
+	    System.out.println("node: " + tree0.getChildren("child1"));
+	    
+	
+	}       
+*/
 }
+
