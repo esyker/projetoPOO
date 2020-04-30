057dc849b9e6a5f9788a9e2d21bdd7be2cc5799e
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-04-30 18:32:29 +0100

    done

--- src/directedTree/DefaultDirectedTree.java
+++ src/directedTree/DefaultDirectedTree.java
@@ -16,6 +16,7 @@ public class DefaultDirectedTree<V> implements DirectedTree<V> {
 	public DefaultDirectedTree() {
 		
 	}
+	
 
 	@Override
 	public void addChild(V parent, V child) {
@@ -94,12 +95,30 @@ public class DefaultDirectedTree<V> implements DirectedTree<V> {
 		
 	}
 	
-	
+	private void fillnode(TreeNode<V> node, SpanningTree<V> s, int index, int previus) {
+			
+		int size=s.getNumbVertices();
+		float[][] matrix=s.getWeightsMatrix();
+		
+		for (int j=0; j < size; j++)
+		{
+			if (matrix[index][j]!=0 && j!=previus)
+			{
+				node.addChild(s.getVertexFromIndex(j));
+				fillnode(node,s, j, index);	
+			}
+		}
+	}
 
 	@Override
 	public void loadFromSpanningTree(SpanningTree<V> s) {
 		
+		int index=s.findHeight();
+		V vertex= s.getVertexFromIndex(index);
 		
+		root=new TreeNode<V>(vertex);
+		
+		fillnode(root, s, index, index);
 		
 	}
 		
