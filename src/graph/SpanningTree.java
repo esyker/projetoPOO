8f8ce51af48af2a45bb4acf5d83aeaa056b9c6ce
lumicrespo <50996757+lumicrespo@users.noreply.github.com>
2020-05-11 18:07:33 +0100

    Update SpanningTree.java

--- src/graph/SpanningTree.java
+++ src/graph/SpanningTree.java
@@ -92,16 +92,13 @@ public class SpanningTree<V> extends AbstractUndirectedWeightedGraph<V> {
         }
         int index=0;
         int ht = depth[0]; 
-        System.out.println("depht0:"+ depth[0]);
         for (i = 1; i < currSize; i++) { 
-        	System.out.println("depht"+ i +":"+ depth[i]);
             if (ht > depth[i]) { 
                 ht = depth[i]; 
                 index=i;
                 
             } 
         } 
-        System.out.println("index:"+ index+"\n");
         return index; 
     }
 
